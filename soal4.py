db2f924b9583d1d0534baf0fb4ee2353209fe525
radit <radityaaydin@gmail.com>
2021-12-13 19:38:39 +0700

    file yang menghilang

--- soal4.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-
-for i in range(a):
-  for j in range(a+1):
-    print("", end=" ")
-  for m in range(i+1):
-    print("*", end=" ")
-  a = a-1
-  print(" ")
--- soal5.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-
-for i in range(a):
-  for j in range(i):
-    print("", end=" ")
-  for m in range(a):
-    print("*", end=" ")
-  a = a-1
-  print(" ")
